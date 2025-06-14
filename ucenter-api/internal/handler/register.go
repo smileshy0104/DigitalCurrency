1430fa85fcd75f027b8819ac0bb4f43508b94818
yuyansong <1259581033@qq.com>
2025-06-14 11:35:21 +0800

    CaptchaDomain相关内容

--- ucenter-api/internal/handler/register.go
+++ ucenter-api/internal/handler/register.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"common"
+	"common/tools"
+	"errors"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
 	"ucenter-api/internal/logic"
@@ -21,18 +23,22 @@ func NewRegisterHandler(svcCtx *svc.ServiceContext) *RegisterHandler {
 
 func (h *RegisterHandler) Register(w http.ResponseWriter, r *http.Request) {
 	var req types.Request
-	//if err := httpx.Parse(r, &req); err != nil {
-	//	httpx.ErrorCtx(r.Context(), w, err)
-	//	return
-	//}
+	if err := httpx.ParseJsonBody(r, &req); err != nil {
+		httpx.ErrorCtx(r.Context(), w, err)
+		return
+	}
 
+	newResult := common.NewResult()
+	if req.Captcha == nil {
+		httpx.OkJsonCtx(r.Context(), w, newResult.Deal(nil, errors.New("人机校验不通过")))
+		return
+	}
+	//获取一下ip
+	req.Ip = tools.GetRemoteClientIp(r)
 	l := logic.NewRegisterLogic(r.Context(), h.svcCtx)
 	resp, err := l.Register(&req)
-	if err != nil {
-		httpx.ErrorCtx(r.Context(), w, err)
-	} else {
-		httpx.OkJsonCtx(r.Context(), w, resp)
-	}
+	result := newResult.Deal(resp, err)
+	httpx.OkJsonCtx(r.Context(), w, result)
 }
 
 func (h *RegisterHandler) SendCode(w http.ResponseWriter, r *http.Request) {
@@ -46,9 +52,4 @@ func (h *RegisterHandler) SendCode(w http.ResponseWriter, r *http.Request) {
 	resp, err := l.SendCode(&req)
 	result := common.NewResult().Deal(resp, err)
 	httpx.OkJsonCtx(r.Context(), w, result)
-	//if err != nil {
-	//	httpx.ErrorCtx(r.Context(), w, err)
-	//} else {
-	//	httpx.OkJsonCtx(r.Context(), w, resp)
-	//}
 }
--- ucenter-api/internal/logic/register_logic.go
+++ ucenter-api/internal/logic/register_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"github.com/jinzhu/copier"
 	"grpc-common/ucenter/types/register"
 	"time"
 	"ucenter-api/internal/svc"
@@ -27,7 +28,12 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 func (l *RegisterLogic) Register(req *types.Request) (resp *types.Response, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	_, err = l.svcCtx.URegisterRpc.RegisterByPhone(ctx, &register.RegReq{})
+
+	regReq := &register.RegReq{}
+	if err := copier.Copy(regReq, req); err != nil {
+		return nil, err
+	}
+	_, err = l.svcCtx.URegisterRpc.RegisterByPhone(ctx, regReq)
 	if err != nil {
 		return nil, err
 	}
