55e806d71a6eec61aac9352c19881920d261d20a
yuyansong <1259581033@qq.com>
2025-06-21 13:31:55 +0800

    feat(market): 初始化市场服务的配置和数据库连接
    - 新增配置文件 conf.yaml，定义了服务名称、监听地址、Etcd、MySQL、Redis和MongoDB的配置
    - 实现了 MySQL 和 MongoDB 的数据库连接功能
    - 添加了服务上下文结构体和初始化函数，集成了配置和数据库连接- 新增了 ExchangeCoin 模型并生成了对应的 GORM 结构体

---
+++ market/internal/svc/service_context.go
@@ -0,0 +1,42 @@
+package svc
+
+import (
+	"common/db"
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"market/internal/config"
+	"market/internal/database"
+)
+
+// ServiceContext 服务上下文结构体
+type ServiceContext struct {
+	Config      config.Config         // 配置文件对象
+	Cache       cache.Cache           // 缓存组件
+	MongoClient *database.MongoClient // mongo
+	Db          *db.DB                // 数据库连接
+}
+
+// NewServiceContext 创建并初始化一个新的服务上下文。
+// 该函数接收一个配置对象作为参数，并基于该配置对象初始化服务上下文中的各个组件。
+// 主要负责初始化缓存组件，使用redis作为缓存存储。
+func NewServiceContext(c config.Config) *ServiceContext {
+	// 初始化Redis缓存组件。
+	// 这里使用了cache.New函数来创建一个新的缓存对象。
+	// 参数分别为：配置对象中的缓存相关配置、nil（表示没有使用额外的中间件）、
+	// 一个统计对象用于监控缓存的性能、nil（表示没有使用额外的插件），
+	// 以及一个初始化缓存选项的函数，这里为空函数，表示使用默认配置。
+	redisCache := cache.New(
+		c.CacheRedis,
+		nil,
+		cache.NewStat("mscoin"),
+		nil,
+		func(o *cache.Options) {})
+	// 初始化MySQL数据库连接。
+	mysql := database.ConnMysql(c.Mysql.DataSource)
+	// 返回新的服务上下文对象，包含配置对象和初始化后的缓存组件。
+	return &ServiceContext{
+		Config:      c,
+		Cache:       redisCache,
+		MongoClient: database.ConnectMongo(c.Mongo),
+		Db:          mysql,
+	}
+}
