0e76440ec3e655c478d10a920ee3433cd57066f3
yuyansong <1259581033@qq.com>
2025-06-21 09:44:06 +0800

    refactor(job-center): 重构任务中心并添加 MongoDB 支持- 在 Kline 结构体中添加 MongoClient 字段，用于支持 MongoDB 操作 - 在 main.go 中添加 MongoDB 客户端断开连接逻辑 - 重构 task.go 文件，定义 Task 结构体和相关方法 - 注释掉部分定时任务，简化代码结构 - 添加 StartBlocking 和 Stop 方法，明确任务启动和停止逻辑

--- job-center/internal/logic/kline.go
+++ job-center/internal/logic/kline.go
@@ -4,6 +4,7 @@ import (
 	"common/tools"
 	"encoding/json"
 	"github.com/zeromicro/go-zero/core/stores/cache"
+	"job-center/internal/database"
 	"log"
 	"sync"
 	"time"
@@ -27,9 +28,10 @@ type OkxResult struct {
 
 // Kline 结构体用于处理K线数据
 type Kline struct {
-	wg sync.WaitGroup // 用于同步控制
-	c  OkxConfig      // OKX配置
-	ch cache.Cache    // 缓存接口
+	wg sync.WaitGroup        // 用于同步控制
+	c  OkxConfig             // OKX配置
+	m  *database.MongoClient // MongoDB客户端
+	ch cache.Cache           // 缓存接口
 }
 
 // Do 方法用于并发获取指定交易对的K线数据。
@@ -84,9 +86,10 @@ func (k *Kline) getKlineData(instId string, symbol string, period string) {
 }
 
 // NewKline 创建Kline实例
-func NewKline(c OkxConfig, cache2 cache.Cache) *Kline {
+func NewKline(c OkxConfig, mongoClient *database.MongoClient, cache2 cache.Cache) *Kline {
 	return &Kline{
 		c:  c,
+		m:  mongoClient,
 		ch: cache2,
 	}
 }
--- job-center/internal/task/task.go
+++ job-center/internal/task/task.go
@@ -7,52 +7,59 @@ import (
 	"time"
 )
 
+// Task 定时任务
+// 该结构体用于管理定时任务，包括任务的创建、运行和停止
 type Task struct {
-	s   *gocron.Scheduler
-	ctx *svc.ServiceContext
+	s   *gocron.Scheduler   // s 是调度器，用于控制任务的执行时间
+	ctx *svc.ServiceContext // ctx 是服务上下文，包含任务执行所需的配置和依赖
 }
 
+// NewTask 创建新的定时任务
 func NewTask(ctx *svc.ServiceContext) *Task {
 	return &Task{
-		s:   gocron.NewScheduler(time.UTC),
+		s:   gocron.NewScheduler(time.UTC), // 创建一个新的调度器，并设置时区为UTC
 		ctx: ctx,
 	}
 }
 
+// Run 启动定时任务
+// 该方法定义了各种时间间隔的定时任务，例如每分钟、每小时、每天等
 func (t *Task) Run() {
+	// 定义各种时间间隔的定时任务，例如每分钟、每小时、每天等
 	t.s.Every(1).Minute().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1m")
+		logic.NewKline(t.ctx.Config.Okx, t.ctx.MongoClient, t.ctx.Cache).Do("1m")
 	})
 	t.s.Every(3).Minute().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("3m")
+		logic.NewKline(t.ctx.Config.Okx, t.ctx.MongoClient, t.ctx.Cache).Do("3m")
 	})
 	t.s.Every(5).Minute().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("5m")
+		logic.NewKline(t.ctx.Config.Okx, t.ctx.MongoClient, t.ctx.Cache).Do("5m")
 	})
-	t.s.Every(15).Minute().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("15m")
-	})
-	t.s.Every(30).Minute().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("30m")
-	})
-	t.s.Every(1).Hour().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1H")
-	})
-	t.s.Every(2).Hour().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("2H")
-	})
-	t.s.Every(4).Hour().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("4H")
-	})
-	t.s.Every(1).Day().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1D")
-	})
-	t.s.Every(1).Week().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1W")
-	})
-	t.s.Every(1).Month().Do(func() {
-		logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1M")
-	})
+	//t.s.Every(15).Minute().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("15m")
+	//})
+	//t.s.Every(30).Minute().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("30m")
+	//})
+	//t.s.Every(1).Hour().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1H")
+	//})
+	//t.s.Every(2).Hour().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("2H")
+	//})
+	//t.s.Every(4).Hour().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("4H")
+	//})
+	//t.s.Every(1).Day().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1D")
+	//})
+	//t.s.Every(1).Week().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1W")
+	//})
+	//t.s.Every(1).Month().Do(func() {
+	//	logic.NewKline(t.ctx.Config.Okx, t.ctx.Cache).Do("1M")
+	//})
+	// 以下任务被注释掉，可在未来启用
 	//t.s.Every(1).Minute().Do(func() {
 	//	logic.NewRate(t.ctx.Config.Okx, t.ctx.Cache).Do()
 	//})
@@ -62,10 +69,14 @@ func (t *Task) Run() {
 	//})
 }
 
+// StartBlocking 启动定时任务并阻塞当前线程
+// 该方法用于启动任务，并保持程序运行，直到任务被停止
 func (t *Task) StartBlocking() {
-	t.s.StartBlocking()
+	t.s.StartBlocking() // 调用调度器的StartBlocking方法来启动任务并阻塞当前线程
 }
 
+// Stop 停止定时任务
+// 该方法用于停止所有正在运行的任务
 func (t *Task) Stop() {
-	t.s.Stop()
+	t.s.Stop() // 调用调度器的Stop方法来停止所有任务
 }
