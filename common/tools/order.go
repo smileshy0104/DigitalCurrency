5b0b7fc82173937748459a2486bdea71478ed97d
yuyansong <1259581033@qq.com>
2025-07-13 22:05:37 +0800

    feat(order): 添加订单添加功能- 实现了 AddOrder 方法，用于将订单信息添加到数据库- 添加了生成唯一订单 ID 的工具函数 Unq - 优化了订单添加流程，包括初始化订单状态、计算冻结资金或币量、检查余额等步骤 -引入了错误处理机制，当余额不足时返回相应错误信息

--- common/tools/order.go
+++ common/tools/order.go
@@ -6,8 +6,20 @@ import (
 	"time"
 )
 
+// Unq 生成一个基于当前时间和随机数的唯一后缀字符串。
+// 这个函数通常用于在基于prefix的基础之上创建唯一的标识符。
+// 参数:
+//
+//	prefix - 前缀字符串，用于个性化生成的唯一字符串。
+//
+// 返回值:
+//
+//	一个格式化的字符串，包含prefix、当前时间和一个随机数。
 func Unq(prefix string) string {
+	// 获取当前时间的Unix毫秒时间戳，用于生成唯一性部分。
 	milli := time.Now().UnixMilli()
+	// 生成一个随机数，进一步增加唯一性。
 	intn := rand.Intn(999999)
+	// 将前缀、当前时间和随机数格式化为一个字符串并返回。
 	return fmt.Sprintf("%s%d%d", prefix, milli, intn)
 }
--- exchange/internal/domain/order_domain.go
+++ exchange/internal/domain/order_domain.go
@@ -2,12 +2,16 @@ package domain
 
 import (
 	"common/db"
+	"common/op"
+	"common/tools"
 	"context"
+	"errors"
 	"exchange/internal/dao"
 	"exchange/internal/model"
 	"exchange/internal/repo"
 	"grpc-common/market/mk_client"
 	"grpc-common/ucenter/uc_client"
+	"time"
 )
 
 // OrderDomain 交易货币模块
@@ -53,10 +57,62 @@ func (d *OrderDomain) FindCurrentTradingCount(ctx context.Context, userId int64,
 }
 
 // AddOrder 添加订单
+// AddOrder 添加订单信息到数据库。
+// 该函数接收一个上下文、数据库连接、订单信息、币种信息以及两个钱包对象作为参数，
+// 并根据订单类型和方向计算所需冻结的资金或币量，最后返回计算结果或错误信息。
+// 参数:
+//
+//	ctx - 上下文，用于传递请求范围的数据、取消信号等。
+//	conn - 数据库连接对象，用于执行数据库操作。
+//	order - 订单信息对象，包含订单的相关数据。
+//	coin - 币种信息对象，包含币种的相关数据。
+//	baseWallet - 主钱包对象，用于买单时扣除资金。
+//	coinWallet - 币种钱包对象，用于卖单时扣除币量。
+//
+// 返回值:
+//
+//	float64 - 计算得到的需冻结的资金或币量。
+//	error - 错误信息，如果执行过程中遇到错误则返回。
 func (d *OrderDomain) AddOrder(ctx context.Context, conn db.DbConn, order *model.ExchangeOrder,
 	coin *mk_client.ExchangeCoin, baseWallet *uc_client.MemberWallet, coinWallet *uc_client.MemberWallet) (float64, error) {
+	// 初始化订单状态、成交量和下单时间
+	order.Status = model.Init
+	order.TradedAmount = 0
+	order.Time = time.Now().UnixMilli()
+	// 生成唯一的订单ID
+	order.OrderId = tools.Unq("E")
 
-	return 0, nil
+	// 交易时暂时不考虑手续费
+	// 定义变量money来存储根据订单类型和方向计算出的需冻结的资金或币量
+	var money float64
+
+	// 根据订单方向判断是买入还是卖出
+	if order.Direction == model.BUY {
+		// 如果是市价单，直接使用订单的amount作为冻结资金
+		if order.Type == model.MarketPrice {
+			money = order.Amount
+		} else {
+			// 如果是限价单，计算冻结资金，这里使用op.MulFloor函数来避免精度损失
+			money = op.MulFloor(order.Price, order.Amount, 8)
+		}
+		// 检查主钱包余额是否足够冻结
+		if baseWallet.Balance < money {
+			return 0, errors.New("余额不足")
+		}
+	} else {
+		// 如果是卖单，直接使用订单的amount作为冻结币量
+		money = order.Amount
+		// 检查币种钱包余额是否足够冻结
+		if coinWallet.Balance < money {
+			return 0, errors.New("余额不足")
+		}
+	}
+
+	// 保存订单
+	err := d.OrderRepo.Save(ctx, conn, order)
+
+	// 函数执行完毕，返回计算出的冻结资金或币量以及nil作为错误信息
+	return money, err
 }
 
 // NewOrderDomain 创建交易货币模块
