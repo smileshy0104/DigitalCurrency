b4df08295e43b896c25a5ab178e7bd2f610c1a9c
yuyansong <1259581033@qq.com>
2025-07-13 17:52:45 +0800

    feat(order): 增加订单创建时的币种和交易限制校验
    - 添加了对交易币种状态的检查，确保币种未被禁用
    -增加了对买入和卖出操作的最小成交额、最大和最小交易量限制的校验- 优化了错误信息的提示，使其更加友好

--- exchange/internal/logic/order_logic.go
+++ exchange/internal/logic/order_logic.go
@@ -6,9 +6,11 @@ import (
 	"exchange/internal/domain"
 	"exchange/internal/model"
 	"exchange/internal/svc"
+	"fmt"
 	"github.com/jinzhu/copier"
 	"github.com/zeromicro/go-zero/core/logx"
 	"grpc-common/exchange/types/order"
+	"grpc-common/market/types/market"
 	"grpc-common/ucenter/types/member"
 )
 
@@ -72,13 +74,56 @@ func (l *OrderLogic) Add(req *order.OrderReq) (*order.AddOrderRes, error) {
 		return nil, err
 	}
 	if memberInfo.TransactionStatus == 0 {
-		return nil, errors.New("此用户已经被禁止交易")
+		return nil, errors.New("此用户已经被禁止交易！")
 	}
 	if req.Type == model.TypeMap[model.LimitPrice] && req.Price <= 0 {
-		return nil, errors.New("限价模式下价格不能小于等于0")
+		return nil, errors.New("限价模式下价格不能小于等于0！")
 	}
 	if req.Amount <= 0 {
-		return nil, errors.New("数量不能小于等于0")
+		return nil, errors.New("购买数量不能小于等于0！")
+	}
+	// 2、通过交易币种名称获取币种信息
+	exchangeCoinInfo, err := l.svcCtx.MarketRpc.FindSymbolInfo(l.ctx, &market.MarketReq{
+		Symbol: req.Symbol, // 交易币种名称，格式：BTC/USDT
+	})
+	if err != nil {
+		return nil, err
+	}
+	if exchangeCoinInfo.Exchangeable != 1 && exchangeCoinInfo.Enable != 1 {
+		return nil, errors.New("该币种被禁用！")
+	}
+	// 获取基准币种，如BTC/USDT中的BTC
+	baseSymbol := exchangeCoinInfo.GetBaseSymbol()
+	// 获取交易币种，如BTC/USDT中的USDT
+	coinSymbol := exchangeCoinInfo.GetCoinSymbol()
+	unit := baseSymbol
+	if req.Direction == model.DirectionMap[model.SELL] {
+		//根据交易币查询
+		unit = coinSymbol
+	}
+	// 查询货币信息
+	coinInfo, err := l.svcCtx.MarketRpc.FindCoinInfo(l.ctx, &market.MarketReq{
+		Unit: unit,
+	})
+	// 如果查询出错或货币信息不存在，返回错误
+	if err != nil || coinInfo == nil {
+		return nil, errors.New("该货币不存在！")
+	}
+
+	// 根据请求类型和方向检查交易限制
+	if req.Type == model.TypeMap[model.MarketPrice] && req.Direction == model.DirectionMap[model.BUY] {
+		// 对于买入操作，检查最小成交额限制
+		if exchangeCoinInfo.GetMinTurnover() > 0 && req.Amount < float64(exchangeCoinInfo.GetMinTurnover()) {
+			return nil, errors.New("成交额至少是" + fmt.Sprintf("%d", exchangeCoinInfo.GetMinTurnover()))
+		}
+	} else {
+		// 对于其他操作，检查最大和最小交易量限制
+		if exchangeCoinInfo.GetMaxVolume() > 0 && exchangeCoinInfo.GetMaxVolume() < req.Amount {
+			return nil, errors.New("数量超出" + fmt.Sprintf("%f", exchangeCoinInfo.GetMaxVolume()))
+		}
+		if exchangeCoinInfo.GetMinVolume() > 0 && exchangeCoinInfo.GetMinVolume() > req.Amount {
+			return nil, errors.New("数量不能低于" + fmt.Sprintf("%f", exchangeCoinInfo.GetMinVolume()))
+		}
 	}
 	return &order.AddOrderRes{
 		OrderId: "hhhhh",
